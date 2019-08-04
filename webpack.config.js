9c36cb7d51c225378035b1d811f2cfeb91ec2c00
Ramiro Andrés <ramiroandres@Ramiros-MacBook-Pro.local>
2019-08-03 21:34:27 -0500

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './app/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js'
+        filename: 'bundle.js'
     },
     module: {
         rules: [
@@ -12,5 +13,10 @@ module.exports = {
             { test: /\.css$/, use: ['style-loader', 'css-loader'] }
         ]
     },
-    mode: 'development'
+    mode: 'development',
+    plugins : [
+        new HtmlWebpackPlugin({
+            template: 'app/index.html'
+        })
+    ]
 }

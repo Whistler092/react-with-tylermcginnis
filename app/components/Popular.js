e3dc10c34cf5848be63f5d6d36d2cadff3110f0d
Ramiro Andrés <iamramiroo@gmail.com>
2019-08-06 13:44:52 -0500

    add propTypes

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+
+function LanguagesNav ({selected , onUpdateLanguage }){
+    const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
+    return (
+      <ul className="flex-center">
+        {languages.map(language => (
+          <li key={language}>
+            <button
+              className="btn-clear nav-link"
+              style={language === selected ? {color: 'rgb(187,46,31)'} : null}
+              onClick={() => onUpdateLanguage(language)}
+            >
+              {language}
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+}
+
+LanguagesNav.propTypes = {
+    selected : PropTypes.string.isRequired,
+    onUpdateLanguage: PropTypes.func.isRequired
+}
 
 export default class Popular extends React.Component {
-    render(){
-        const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
-        return (
-            <ul className="flex-center">
-                {languages.map((language) => (
-                    <li key={language}>
-                        <button className="btn-clear nav-link">
-                            {language}
-                        </button>
+  constructor(props) {
+    super(props);
 
-                    </li>
-                ))}
-            </ul>
-        )
-    }
+    this.state = {
+      selectedLanguage: "All"
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    });
+  }
+
+  render() {
+
+    const { selectedLanguage } = this.state
+    return (<React.Fragment>
+        <LanguagesNav 
+            selected={selectedLanguage}
+            onUpdateLanguage={this.updateLanguage}
+        />
+    </React.Fragment>)
+  }
 }
